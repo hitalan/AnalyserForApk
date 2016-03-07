18167f45fe47d0737f73bc9cff5f0db5278202fd
hit_alan <hit_software_alan@163.com>
2016-03-07 22:08:51 +0800

    write the shell of get the two file md5 value and make whether there is a diffrence

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+the_compare_result=$(python testforFile.py $1 $2)
+echo $the_compare_result
--- testforFile.py
+++ testforFile.py
@@ -1,12 +1,22 @@
 #coding=utf-8
 from hashlib import md5
-  
+import sys 
 def md5_file(name):
     m = md5()
-    a_file = open(name, 'rb')    #��Ҫʹ�ö����Ƹ�ʽ��ȡ�ļ�����
+    a_file = open(name, 'rb')
     m.update(a_file.read())
     a_file.close()
     return m.hexdigest()
   
- 
-print md5_file('a.txt')
+first = md5_file(sys.argv[1])
+second = md5_file(sys.argv[2])
+if first==second:
+	print	'yes'
+else:
+	print	'wrong'
+
+print sys.argv[1]
+print sys.argv[2]
+print md5_file(sys.argv[1])
+
+print md5_file(sys.argv[2])
